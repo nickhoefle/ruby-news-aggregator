eadacd7427a90ca353aa8db205899239ce005e55
Nick <nickhoefle@gmail.com>
2023-07-16 19:58:50 -0500

    Added dark mode color scheme; aligned news article layouts; removed all inline css.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -15,28 +15,34 @@ class DataController < ApplicationController
     doc = REXML::Document.new(xml_data)
     items = []
     doc.elements.each('rss/channel/item') do |item|
+      channel = doc.elements['rss/channel']
+      channel_image_url = extract_channel_image_url(channel)
       item_data = {
         title: item.elements['title'].text,
         description: item.elements['description'].text,
         link: extract_link(item),
         pub_date: item.elements['pubDate'].text,
-        image_url: extract_image_url(item)
+        image_url: extract_image_url(item),
+        channel_image_url: channel_image_url
       }
       items << item_data
     end
     items
   end
-  #Check for <link> or <guid>
+
   def extract_link(item)
     item.elements['link']&.text || item.elements['guid']&.text
   end
-  
+
   def extract_image_url(item)
     image_url = item.elements['image']&.attributes&.fetch('url', nil)
     image_url ||= item.elements['enclosure']&.attributes&.fetch('url', nil)
     image_url ||= item.elements['media:content']&.attributes&.fetch('url', nil)
     image_url
   end
-  
-  
+
+  def extract_channel_image_url(channel)
+    channel.elements['image/url'].text
+  end
 end
+
